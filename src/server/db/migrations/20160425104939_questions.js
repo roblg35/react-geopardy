e4cbcf1f3bf09637609640efc4c054f77b2f4924
millsmcilroy <millsmcilroy@gmail.com>
2016-04-25 12:56:00 -0600

    schema complete

--- src/server/db/migrations/20160425104939_questions.js
+++ src/server/db/migrations/20160425104939_questions.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('questions', function(table){
+    table.increments();
+    table.text('question');
+    table.text('answer');
+    table.integer('points');
+    table.boolean('daily_double');
+    table.integer('game_id').references('id').inTable('games').onDelete('CASCADE');
+    table.integer('category_id').references('id').inTable('categories').onDelete('CASCADE');
+  });
+
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('questions');
 };
