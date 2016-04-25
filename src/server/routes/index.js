5bb7783b7b4556fe04c4ca8cf88c714ef1730c8f
rlgaines <robertlg35@gmail.com>
2016-04-25 17:59:12 -0600

    allowed cors from frontend http request

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.sendFile('./index.html');
+router.get('/ping', function(req, res, next) {
+  res.json({ questions: [{ a: 3 }] });
+  // res.sendFile('./index.html');
 });
 
 module.exports = router;
